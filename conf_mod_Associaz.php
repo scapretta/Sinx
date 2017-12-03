edc0dc5b35b883e31f979de7027a08f21f1462fe
Sinx for Association <sergio.capretta@gmail.com>
2017-12-03 13:55:53 +0100

    Add files via upload
    Inserite nuove voci d'informazione della propria Associazione (Grazie al suggerimento di Ivano Bucelli)

--- conf_mod_Associaz.php
+++ conf_mod_Associaz.php
@@ -32,7 +32,18 @@ if ($user == 'admin') {
 	$cf = $_POST['cf'];
 	$email = $_POST['email'];
 	$webmail = $_POST['webmail'];
+	$PEC = $_POST['PEC'];
+	$webPEC = $_POST['webPEC'];
 	$sito = $_POST['sito'];
+	$facebook = $_POST['facebook'];
+	$instagram = $_POST['instagram'];
+	$twitter = $_POST['twitter'];
+	$youtube = $_POST['youtube'];
+	$banca = $_POST['banca'];
+	$IBAN = $_POST['IBAN'];
+	$BIC = $_POST['BIC'];
+	$HomeBanking = $_POST['HomeBanking'];
+	$IscrizioneODVoAPS = $_POST['IscrizioneODVoAPS'];
 
 
 //Funzione per il redirect
@@ -54,7 +65,7 @@ function redirect($url,$tempo = FALSE ){
 //CONTROLLO DEI CAMPI
 
  
-		$sql="UPDATE tb_anagrafe_associaz SET nome='$nome', indirizzo='$indirizzo', numero='$numero', cap='$cap', citta='$citta', provincia='$provincia', tel='$tel', fax='$fax', cf='$cf', email='$email', webmail='$webmail', sito='$sito' WHERE id_anagrafe = '1'"; //inserisco i valori nel database
+		$sql="UPDATE tb_anagrafe_associaz SET nome='$nome', indirizzo='$indirizzo', numero='$numero', cap='$cap', citta='$citta', provincia='$provincia', tel='$tel', fax='$fax', cf='$cf', email='$email', webmail='$webmail', PEC='$PEC', webPEC='$webPEC', sito='$sito', facebook='$facebook', instagram='$instagram', twitter='$twitter', youtube='$youtube', banca='$banca', IBAN='$IBAN', BIC='$BIC', HomeBanking='$HomeBanking', IscrizioneODVoAPS='$IscrizioneODVoAPS' WHERE id_anagrafe = '1'"; //inserisco i valori nel database
 		$result=mysqli_query($connect, $sql);
 
 	if (!$result) {
